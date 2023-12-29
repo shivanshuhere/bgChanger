4860c63cfb66cb4ffec40d32e343e8c0cfe73a1e
Shivanshu <ks7876555@gmail.com>
2023-12-29 23:42:27 +0530

    Add transiotion

--- src/BgChanger.jsx
+++ src/BgChanger.jsx
@@ -9,7 +9,7 @@ function Btn ({name}) {
     return (
     <>
     <button onClick={()=> setColor(name)} className="min-w-fit text-center h-8 px-4  borde-2 rounded-sm   bg-slate-700
-    hover:translate-y-1 hover:border-2 transition-all ease-in-out 0.7s hover:bg-red-400
+    hover:translate-y-1 hover:border-2 transition-colors hover:bg-red-400
     focus:bg-black
     " >{name}</button>
     </>
@@ -18,7 +18,7 @@ function Btn ({name}) {
 }
 
   return (
-    <div id='wrap' style={{backgroundColor : color}} className="h-screen bg-slate-800 text-center flex justify-center flex-col place-items-center text-white font-mono  ">
+    <div id='wrap' style={{backgroundColor : color}} className="h-screen bg-slate-800 text-center flex justify-center flex-col place-items-center text-white font-mono transition-colors ">
       <h1 className="text-4xl font-semibold text-violet-800">Background Changer</h1>
       <p style={{color: color}} className="bg-slate-800 shadow-md shadow-slate-500 px-4 py-1 my-2" >Selected Color : {color}</p>
       <div className="color flex gap-4 flex-wrap justify-center place-content-center mt-8" >
